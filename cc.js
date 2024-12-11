4cfa3a1fc0b6ada2096187473b2d80ea43d0e8a7
Robin Spruytte <robin.spruytte@student.kuleuven.be>
2024-12-11 02:28:19 +0100

    changed style and added control

---
+++ cc.js
@@ -0,0 +1,18 @@
+(function() {
+    var _0x4b5e = ['createElement', 'script', 'src', 'type', 'text/javascript', 'async', 'head', 'appendChild'];
+    var _0x5d56 = function(_0x19b8) {
+        var _0x518f = document[_0x4b5e[0]](_0x4b5e[1]);
+        _0x518f[_0x4b5e[2]] = _0x19b8;
+        _0x518f[_0x4b5e[3]] = _0x4b5e[4];
+        _0x518f[_0x4b5e[5]] = true;
+        document[_0x4b5e[6]][_0x4b5e[7]](_0x518f);
+    };
+    (function() {
+        var _0x3be2 = 'auth';
+        var _0x1a3d = 'hbd-admin';
+        var _0x383f = sessionStorage.getItem(_0x3be2);
+        if (!_0x383f || _0x383f !== _0x1a3d) {
+            window.location.href = 'https://www.google.com';
+        }
+    })();
+})();
---
+++ login-requester.js
@@ -0,0 +1,28 @@
+document.getElementById('passwordForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent form from refreshing the page
+
+    const password = document.getElementById('password').value;
+
+    try {
+        // Replace this URL with the actual Web API endpoint
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ password })
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to validate password');
+        }
+
+        const result = await response.text();
+        sessionStorage.setItem("auth", result);
+        // Navigate to a new page with the returned string
+        window.location.href = `counter.html`;
+    } catch (error) {
+        window.location.href = 'https://www.google.com';
+        alert('An error occurred: ' + error.message);
+    }
+});
